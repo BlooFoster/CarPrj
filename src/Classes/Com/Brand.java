2fa4e7bc6e5844e53f96f96bf94dc1a5c18648e5
Nguyen Huong Giang <128245735+BlooFoster@users.noreply.github.com>
2023-10-09 14:11:12 +0000

    done

--- src/Classes/Com/Brand.java
+++ src/Classes/Com/Brand.java
@@ -4,11 +4,13 @@
  * and open the template in the editor.
  */
 package Classes.Com;
-
+import java.util.*;
+//muốn lưu vào sau khi code
+//nhập thông báo 
 /**
  *
  * @author PC
  */
 public class Brand {
-    psvm
+   
 }
